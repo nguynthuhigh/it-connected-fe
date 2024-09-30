bc325314f5d42d08ab5dd62aa13f88f6380ed60a
phucpham13123 <phucpham21109@gmail.com>
2024-10-01 00:10:45 +0700

    update dashboard

--- src/admin/components/manager-user/card-view.tsx
+++ src/admin/components/manager-user/card-view.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import IconStonk from '../../assets/svg/IconStonk.svg';
 
 interface Person {
--- src/admin/pages/home/dashboard.tsx
+++ src/admin/pages/home/dashboard.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+
+import Header from '../../components/homepage/header';
+import Search from '../../components/homepage/search';
+import IconListRed from '../../assets/svg/iconList.svg';
+import IconNextRed from '../../assets/svg/iconNextRed.svg';
+
+import TopEmployer from '../../components/homepage/top-employer';
+import Prepare from '../../components/homepage/prepare';
+import Footer from '../../components/homepage/footer';
+
+
+
 
 const Dashboard = () => {
+
   return (
-    <div>Dashboard</div>
+    <div className="font-inter">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Search />
+      </div>
+
+      <div className='bg-[#f7f7f7] py-6 mb-12 border-b'>
+        <div className='flex max-w-[1200px] mx-auto justify-center gap-5 text-[16px] items-center'> 
+          <img className='w-[22px]' src={IconListRed} alt="iconList" />
+          <div className='font-semibold'>
+            Khảo sát lương & tuyển dụng IT
+          </div>
+          <div className=''>
+            ĐỊNH HÌNH THỊ TRƯỜNG IT 2024-2025 BẰNG Ý KIẾN CỦA BẠN
+          </div>
+          <img src={IconNextRed} alt="iconNext" />
+        </div>
+      </div>
+        
+      <div>
+        <Prepare />
+      </div>
+      
+      <div>
+        <TopEmployer />
+      </div>
+      
+      <div>
+        <Footer />
+      </div>
+
+    </div>
   )
 }
 
-export default Dashboard
+
+export default Dashboard;
